a233e90542db3f0119abcd3e5e1f6214e3a49a59
Pratiksha Ramesh Anand <66176068+anandpratiksha@users.noreply.github.com>
2021-07-16 00:10:43 +0530

    clone

--- src/component/Header.js
+++ src/component/Header.js
@@ -3,7 +3,7 @@ import './Header.css'
 const Header = () => {
     return (
         <div>
-            <span className="header">Entertainment Hub</span>
+            <span className="header">Movies Reviews, Rating Hub</span>
 
         </div>
     )
