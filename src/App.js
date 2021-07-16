4a64da4a5157512e4ad0e7a79e9bd1caac939554
anandpratiksha <apratiksha1137@gmail.com>
2021-07-16 22:07:42 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './component/Header'
 import BottomNavigation from './component/MainNav'
+import Trending from './Pages/Trending/Trending'
+import Movies from './Pages/Movies/Movies'
+import Series from './Pages/Series/Series'
+import Search from './Pages/Search/Search'
+import { Container } from '@material-ui/core'
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <div className="App">
-        <div>
+        <Container>
           <Switch>
             <Route path="/" component={Trending} exact />
             <Route path="/movies" component={Movies} />
             <Route path="/series" component={Series} />
             <Route path="/search" component={Search} />
           </Switch>
-        </div>
+        </Container>
       </div>
       <BottomNavigation />
     </BrowserRouter>
---
+++ src/Pages/Series/Series.js
@@ -0,0 +1,9 @@
+const Series = () => {
+    return (
+        <div>
+            <span className="pageTitle">TV Series</span>
+        </div>
+    )
+}
+
+export default Series
