c44a9a9e1f1842d59aa8152c2af06ce12861f468
anandpratiksha <apratiksha1137@gmail.com>
2021-07-17 23:54:48 +0530

    updates

--- src/component/Header.js
+++ src/component/Header.js
@@ -3,8 +3,7 @@ import './Header.css'
 const Header = () => {
     return (
         <div>
-            <span className="header">Movies Reviews, Rating Hub</span>
-
+            <span onClick={() => window.scroll(0, 0)} className="header">Movies Reviews, Rating Hub</span>
         </div>
     )
 }
