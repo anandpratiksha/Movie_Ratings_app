085d6bc4b2845c3989d27609b87d7a42591b2ba7
anandpratiksha <apratiksha1137@gmail.com>
2021-07-17 21:55:18 +0530

    updates

--- src/component/SingleContent/SingleContent.js
+++ src/component/SingleContent/SingleContent.js
@@ -1,7 +1,9 @@
+import { img_300 } from '../../Config/config'
+
 const SingleContent = ({ id, poster, title, date, media_type, vote_average }) => {
     return (
         <div>
-            {title}
+            <img src={`${img_300}/${poster}`} />
         </div>
     )
 }
