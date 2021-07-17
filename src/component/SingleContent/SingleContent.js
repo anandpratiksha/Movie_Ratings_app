068710b88c9b78ab71f6c94778f3040f31bc14d8
anandpratiksha <apratiksha1137@gmail.com>
2021-07-18 00:09:00 +0530

    updates

--- src/component/SingleContent/SingleContent.js
+++ src/component/SingleContent/SingleContent.js
@@ -1,9 +1,13 @@
 import { img_300, unavailable } from '../../Config/config';
 import "./SingleContent.css"
+import Badge from '@material-ui/core/Badge';
 
 const SingleContent = ({ id, poster, title, date, media_type, vote_average }) => {
     return (
         <div className="media">
+            <Badge badgeContent={vote_average}
+                color={vote_average > 6 ? 'primary' : 'error'}
+            />
             <img className="poster" src={poster ? `${img_300}/${poster}` : unavailable} alt={title} />
             <b className="title">{title}</b>
             <span className="subTitle">{media_type === 'tv' ? "TV Series" : "Movies"}
