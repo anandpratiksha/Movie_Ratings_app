6c0153276aa033b9bd945bc25f54c4a0f52b65b1
anandpratiksha <apratiksha1137@gmail.com>
2021-07-17 22:34:03 +0530

    updates

--- src/component/SingleContent/SingleContent.js
+++ src/component/SingleContent/SingleContent.js
@@ -1,9 +1,14 @@
-import { img_300 } from '../../Config/config'
+import { img_300, unavailable } from '../../Config/config';
+import "./SingleContent.css"
 
 const SingleContent = ({ id, poster, title, date, media_type, vote_average }) => {
     return (
-        <div>
-            <img src={`${img_300}/${poster}`} />
+        <div className="media">
+            <img className="poster" src={poster ? `${img_300}/${poster}` : unavailable} alt={title} />
+            <b className="title">{title}</b>
+            <span className="subTitle">{media_type === 'tv' ? "TV Series" : "Movies"}
+                <span className="subTitle">{date}</span>
+            </span>
         </div>
     )
 }
