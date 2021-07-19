6407b3710a45a2812b32fbb612be0dff6f6a216e
anandpratiksha <apratiksha1137@gmail.com>
2021-07-19 23:43:20 +0530

    updates

--- src/Pages/Movies/Movies.js
+++ src/Pages/Movies/Movies.js
@@ -1,13 +1,44 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react';
+import CustomPagination from '../../component/Pagination/CustomPagination';
+import SingleContent from '../../component/SingleContent/SingleContent';
+
 const Movies = () => {
 
-    const fetchMovies = () => {
-        const { data } = await axios.get("");
-    }
+    const [page, setPage] = useState(1);
+    const [content, setContent] = useState([]);
+    const [numOfPages, setNumOfPages] = useState();
 
+    const fetchMovies = async () => {
+        const { data } = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=true&page=${page}&with_watch_monetization_types=flatrate`);
+        setContent(data.results);
+        setNumOfPages(data.total_pages)
+    };
+
+    useEffect(() => {
+        fetchMovies();
+    }, [page])
     return (
         <div>
             <span className="pageTitle">Movies</span>
+            <div className="trending">
+                {
+                    content && content.map((c) => (
+                        <SingleContent key={c.id}
+                            id={c.id}
+                            poster={c.poster_path}
+                            title={c.title || c.name}
+                            date={c.first_air_date || c.release_date}
+                            media_type='movies'
+                            vote_average={c.vote_average}
 
+                        />
+                    ))
+                }
+            </div>
+            {numOfPages > 1 && (
+                <CustomPagination setPage={setPage} numOfPages={numOfPages} />
+            )}
         </div>
     )
 }
