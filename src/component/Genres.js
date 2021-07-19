70afd41da757cbf78657ca82452153d303acd8e4
anandpratiksha <apratiksha1137@gmail.com>
2021-07-20 00:45:08 +0530

    updates

--- src/component/Genres.js
+++ src/component/Genres.js
@@ -1,4 +1,29 @@
-const Genres = () => {
+import { useEffect } from "react";
+import axios from 'axios'
+
+const Genres = ({
+    selectedGenres,
+    setSelectedGenres,
+    genres,
+    setGenres,
+    type,
+    setPage
+}) => {
+
+    const fetchGenres = async () => {
+        const { data } = await axios.get(`https://api.themoviedb.org/3/genre/${type}/list?api_key=78a839500ec15706362a7cc86bff30d3&language=en-US`)
+        setGenres(data.genres);
+    };
+
+    console.log(genres);
+
+    useEffect(() => {
+        fetchGenres();
+        return () => {
+            setGenres({});
+        };
+    }, [])
+
     return (
         <div>
             Genres
