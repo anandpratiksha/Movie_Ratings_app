435a9754d1dd380033cd3ab8af2897628b7999a6
anandpratiksha <apratiksha1137@gmail.com>
2021-07-25 21:20:43 +0530

    updates

--- src/component/ContentModal/ContentModal.js
+++ src/component/ContentModal/ContentModal.js
@@ -1,31 +1,42 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
-import Backdrop from '@material-ui/core/Backdrop';
-import Fade from '@material-ui/core/Fade';
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+import axios from "axios";
+import {
+    img_500,
+    unavailable,
+    unavailableLandscape,
+} from "../../Config/config";
+import "./ContentModal.css";
+import { Button } from "@material-ui/core";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import Carousel from "../Carousel/Carousel";
 
 const useStyles = makeStyles((theme) => ({
     modal: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
     },
     paper: {
         width: "90%",
         height: "80%",
-        backgroundColor: '#394450',
-        borderRadius: ' 10',
-        color: 'white',
-        // backgroundColor: theme.palette.background.paper,
-        // border: '2px solid #000',
+        backgroundColor: "#39445a",
+        border: "1px solid #282c34",
+        borderRadius: 10,
+        color: "white",
         boxShadow: theme.shadows[5],
-        padding: theme.spacing(2, 4, 3),
+        padding: theme.spacing(1, 1, 3),
     },
 }));
 
-export default function ContentModal({ children, media_type, id }) {
+export default function TransitionsModal({ children, media_type, id }) {
     const classes = useStyles();
-    const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = useState(false);
+    const [content, setContent] = useState();
+    const [video, setVideo] = useState();
 
     const handleOpen = () => {
         setOpen(true);
@@ -35,11 +46,39 @@ export default function ContentModal({ children, media_type, id }) {
         setOpen(false);
     };
 
+    const fetchData = async () => {
+        const { data } = await axios.get(
+            `https://api.themoviedb.org/3/${media_type}/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+        );
+
+        setContent(data);
+        // console.log(data);
+    };
+
+    const fetchVideo = async () => {
+        const { data } = await axios.get(
+            `https://api.themoviedb.org/3/${media_type}/${id}/videos?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+        );
+
+        setVideo(data.results[0]?.key);
+    };
+
+    useEffect(() => {
+        fetchData();
+        fetchVideo();
+        // eslint-disable-next-line
+    }, []);
+
     return (
-        <div>
-            <button type="button" className="media" onClick={handleOpen}>
+        <>
+            <div
+                className="media"
+                style={{ cursor: "pointer" }}
+                color="inherit"
+                onClick={handleOpen}
+            >
                 {children}
-            </button>
+            </div>
             <Modal
                 aria-labelledby="transition-modal-title"
                 aria-describedby="transition-modal-description"
@@ -53,12 +92,64 @@ export default function ContentModal({ children, media_type, id }) {
                 }}
             >
                 <Fade in={open}>
-                    <div className={classes.paper}>
-                        <h2 id="transition-modal-title">Transition modal</h2>
-                        <p id="transition-modal-description">react-transition-group animates me.</p>
-                    </div>
+                    {content && (
+                        <div className={classes.paper}>
+                            <div className="ContentModal">
+                                <img
+                                    src={
+                                        content.poster_path
+                                            ? `${img_500}/${content.poster_path}`
+                                            : unavailable
+                                    }
+                                    alt={content.name || content.title}
+                                    className="ContentModal__portrait"
+                                />
+                                <img
+                                    src={
+                                        content.backdrop_path
+                                            ? `${img_500}/${content.backdrop_path}`
+                                            : unavailableLandscape
+                                    }
+                                    alt={content.name || content.title}
+                                    className="ContentModal__landscape"
+                                />
+                                <div className="ContentModal__about">
+                                    <span className="ContentModal__title">
+                                        {content.name || content.title} (
+                    {(
+                                            content.first_air_date ||
+                                            content.release_date ||
+                                            "-----"
+                                        ).substring(0, 4)}
+                    )
+                  </span>
+                                    {content.tagline && (
+                                        <i className="tagline">{content.tagline}</i>
+                                    )}
+
+                                    <span className="ContentModal__description">
+                                        {content.overview}
+                                    </span>
+
+                                    <div>
+                                        <Carousel id={id} media_type={media_type} />
+                                    </div>
+
+                                    <Button
+                                        variant="contained"
+                                        startIcon={<YouTubeIcon />}
+                                        color="secondary"
+                                        target="__blank"
+                                        href={`https://www.youtube.com/watch?v=${video}`}
+                                    >
+                                        Watch the Trailer
+                  </Button>
+                                </div>
+                            </div>
+                        </div>
+                    )}
                 </Fade>
             </Modal>
-        </div>
+        </>
     );
 }
